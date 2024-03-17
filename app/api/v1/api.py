2be820626c73d73b4f1313d0ce31b40aa8d7a20b
Nazaroff <visnazarov@greenatom.ru>
2024-03-17 12:40:46 +0300

    fix

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import file, auth, user, order, message
+from app.api.v1.endpoints import file, auth, user, order
 
 api_router = APIRouter()
 api_router.include_router(file.router, prefix="/v1/file", tags=["File"])
 api_router.include_router(auth.router, prefix="/v1/auth", tags=["auth"])
 api_router.include_router(user.router, prefix="/v1/user", tags=["user"])
 api_router.include_router(order.router, prefix="/v1/order", tags=["order"])
-api_router.include_router(message.router, prefix="/v1/message", tags=["message"])
+# api_router.include_router(message.router, prefix="/v1/message", tags=["message"])
--- app/api/v1/endpoints/order.py
+++ app/api/v1/endpoints/order.py
@@ -4,6 +4,7 @@ from loguru import logger
 from app.api.v1.endpoints.auth import get_current_user
 from app.db.db_crud import get_order_by_id
 from app.db.models import OrderStatus
+from app.schemas.message_schemas import MSGIn
 from app.schemas.order_schemas import BaseOrder, OrderWithText
 from app.services.exceptions import handle_domain_error
 from app.services.use_case.index import get_file_by_id
@@ -59,3 +60,44 @@ async def get_order(order_id: str, user=Depends(get_current_user)):
         highlights=highlights,
         persons=persons
     )
+
+@router.get("/message")
+@handle_domain_error
+async def get_user_orders(payload: MSGIn = Depends(), user=Depends(get_current_user)) -> str:
+    # order = await get_order_by_id(payload.order_id)
+    # if order.status != OrderStatus.done:
+    #     raise HTTPException(status_code=status.HTTP_425_TOO_EARLY, detail="Заказ еще обрабатывается")
+    file = get_file_by_id(payload.order_id)
+    text = "Какое то текст"
+    highlights = ["Сделай ....", "Выполни ..."]
+    persons = ["Ваня В", "Кирилл", "Дмитрий"]
+    summary = [{}]
+    if file:
+        file = file[0]
+        source = file["_source"]
+        text = source.get("content")
+        persons = source.get('persons')
+        summary = source.get('summary')
+        highlights = file.get("highlight")
+        if highlights:
+            highlights = highlights['content']
+
+    if payload.message == "Какие были поручения?":
+        return "\n".join(highlights)
+
+    elif payload.message == "О чем был текст?":
+        for summ in summary:
+            if summ.get("name") == "All":
+                return summ.get("text", "")
+
+    elif payload.message == "Кто что сказал?":
+        res = []
+        for summ in summary:
+            if summ.get("name") != "All":
+                res.append(f"{summ.get("name", "Кто то")}: \n {summ.get("text", "")}")
+        return "\n".join(res)
+
+    elif payload.message == "Кто присутствовал на встрече?":
+        return "\n".join(highlights)
+
+    return "Я не знаю такую команду"
